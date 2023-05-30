44c2c55a426164588377fff41a01ae527b126f56
Anandagadugin <gaduginanand984@gmail.com>
2023-05-30 21:10:27 +0530

    created a collapse hamburger menu feature

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,23 +1,38 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { toggleMenu } from "../utils/appSlice";
 
 const Head = () => {
+  const dispatch = useDispatch();
+  const toggleMenuHandler = () => {
+    dispatch(toggleMenu());
+  };
   return (
     <div className="grid grid-flow-col p-3 m-2 shadow-md">
       <div className="flex">
         <img
-          className="h-12 col-span-1"
+          onClick={() => toggleMenuHandler()}
+          className="h-12 col-span-1 cursor-pointer"
           alt="menu"
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcyYIYjMqOdObTIuRhfemvIb9mc0JPmbmdoqgvLoYV-Vtqbpe1MtH3he8Z9acNT5mP1u4&usqp=CAU"
         />
-        <img
-          className="h-12"
-          alt="youtube-logo"
-          src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500"
-        />
+        <a href="/">
+          <img
+            className="h-12"
+            alt="youtube-logo"
+            src="https://lh3.googleusercontent.com/3zkP2SYe7yYoKKe47bsNe44yTgb4Ukh__rBbwXwgkjNRe4PykGG409ozBxzxkrubV7zHKjfxq6y9ShogWtMBMPyB3jiNps91LoNH8A=s500"
+          />
+        </a>
       </div>
+
       <div className="col-span-10 m-3">
-        <input className="w-1/2 border rounded-l-full border-gray-400" type="text " />
-        <button className="border px-2 border-gray-400 rounded-r-full">Search</button>
+        <input
+          className="w-1/2 border rounded-l-full border-gray-400"
+          type="text "
+        />
+        <button className="border px-2 border-gray-400 rounded-r-full">
+          Search
+        </button>
       </div>
       <div className="col-span-1">
         <img
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
+  const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
+
+  // Early return pattern
+  if (!isMenuOpen) return null;
   return (
     <div className="p-5 shadow-md w-58">
-
       <ul>
         <li>History</li>
         <li>Shorts</li>
