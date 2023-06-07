1006ef2d6e49c6cb9eca15348dc931348789f4a6
Anandagadugin <gaduginanand984@gmail.com>
2023-06-07 12:48:44 +0530

    CSS retouch for side bar list

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,30 +1,175 @@
 import React from "react";
-import Button from "./Buttons";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 
 const ButtonList = () => {
-  const ButtonNames = [
-    "All",
-    "Comedy",
-    "Sports",
-    "Virat",
-    "Football",
-    "Movies",
-    "Cooking",
-    "IPL2023",
-    "Asian",
-    "Cars",
-    "Vlogs",
-    "Raps",
-    "Popular",
-    "Beauty",
-    "Tech",
-    "New",
-  ];
+  const slideLeft = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider");
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
-    <div className="flex font-serif ">
-      {ButtonNames.map((name, index) => (
-        <Button key={index} name={name} />
-      ))}
+    <div className="grid grid-flow-col bg-white">
+      <MdChevronLeft
+        className="hover:scale-150 my-auto opacity-50 "
+        size={30}
+        onClick={slideLeft}
+        aria-hidden="true"
+      />
+      <div
+        id="slider"
+        className="flex h-18 m-2 overflow-hidden whitespace-nowrap scroll-smooth"
+      >
+        <div>
+          <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+            All
+          </button>
+        </div>
+        <a href={"/results?search_query=Games"}>
+          <div>
+            <button className=" dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Games
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Music"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Music
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Live"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Live
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Comedy"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Comedy
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Movies"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Movies
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Gadgets"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Gadgets
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=News"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              News
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Musicals"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Musicals
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Trending"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Trending
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Raftaar"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Raftaar
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Drama"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Drama
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Yo+Yo+Honey+Singh"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Yo Yo Honey Singh
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Theater"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Theater
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Cinema"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Cinema
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Reality+Shows"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Reality Shows
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Motivation"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Motivation
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Hip-Hop"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Hip-Hop
+            </button>
+          </div>
+        </a>
+        <a href={"/results?search_query=Technology"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Technology
+            </button>
+          </div>
+        </a>
+
+        <a href={"/results?search_query=Recently+uploaded"}>
+          <div>
+            <button className="dark:bg-white rounded-md shadow-md hover:scale-110 inline-block font-normal m-2 py-1 px-3">
+              Recently uploaded
+            </button>
+          </div>
+        </a>
+      </div>
+      <MdChevronRight
+        className="hover:scale-150 my-auto opacity-50 "
+        size={30}
+        onClick={slideRight}
+        aria-hidden="true"
+      />
     </div>
   );
 };
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,40 +1,75 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FaHome, FaPlay, FaArchive, FaYoutubeSquare, FaHistory, FaYoutube, FaClock, FaThumbsUp, FaFire, FaShoppingBag, FaMusic, FaFilm, FaPodcast, FaGamepad, FaTrophy } from "react-icons/fa";
 
 const Sidebar = () => {
   const isMenuOpen = useSelector((store) => store.app.isMenuOpen);
-
-  // Early return pattern
   if (!isMenuOpen) return null;
   return (
-    <div className="p-5 shadow-md w-58">
-      <ul>
-        <li>
-          <Link to={"/"}>Home</Link>
-        </li>
-        <li>History</li>
-        <li>Shorts</li>
-        <li>Liked videos</li>
-        <li>Watch later</li>
-      </ul>
-      <h2 className="font-bold pt-4">Subscriptions</h2>
-      <ul>
-        <li>Music</li>
-        <li>Sports</li>
-        <li>Gaming</li>
-        <li>Movies</li>
-      </ul>
-      <h2>Explore</h2>
-      <ul>
-        <h2 className="font-bold pt-4">Trending</h2>
-        <li>Shopping</li>
-        <li>Music</li>
-        <li>Learning</li>
-        <li>Fashion & Beauty</li>
-      </ul>
-    </div>
+    <>
+      <div className="p-6 pt-3 shadow-lg w-56 relative bg-white">
+        <ul className="mb-3">
+        <li className="py-3 font-bold cursor-pointer">
+            <Link to={"/"} className="flex items-center">
+              <FaHome className="mr-3" /> Home
+            </Link>
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaPlay className="mr-3" /> Shorts
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaArchive className="mr-3" /> Subscriptions
+          </li>
+        </ul>
+        <hr />
+        <ul className="mb-3 mt-3">
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaYoutube className="mr-3" /> Library
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaHistory className="mr-3" /> History
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaYoutubeSquare className="mr-3" /> Your videos
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaClock className="mr-3" /> Watch later
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaThumbsUp className="mr-3" /> Liked videos
+          </li>
+        </ul>
+        <hr />
+        <h1 className="mt-3 mb-3 font-semibold">Explore</h1>
+        <ul>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaFire className="mr-3" /> Trending
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaShoppingBag className="mr-3" /> Shopping
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaMusic className="mr-3" /> Music
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaFilm className="mr-3" /> Movies
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaPodcast className="mr-3" /> Live
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaGamepad className="mr-3" /> Gaming
+          </li>
+          <li className="py-3 cursor-pointer flex items-center">
+            <FaTrophy className="mr-3" /> Sports
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
 export default Sidebar;
+
+
