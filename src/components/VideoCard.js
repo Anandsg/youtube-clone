1550d9926e397dbcc1223f12ed9bae89c9d78c6c
Anandagadugin <gaduginanand984@gmail.com>
2023-06-07 16:32:42 +0530

    added views with millions and thousands format

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { prettifyNumber } from "../number";
 
 const VideoCard = ({ info }) => {
   // console.log(info);
@@ -9,18 +10,18 @@ const VideoCard = ({ info }) => {
 
   const { snippet, statistics } = info;
   const { channelTitle, title, thumbnails } = snippet;
+  const views = statistics.viewCount;
 
   return (
-    <div className="p-2 m-2 w-64 shadow-md">
+    <div className="p-2 m-2 w-56 shadow-md">
       <img className="rounded-md" alt="video" src={thumbnails.medium.url} />
       <ul>
-        <li className="font-semibold py-2">{title}</li>
+        <li className="font-semibold">{title}</li>
         <li className="">{channelTitle}</li>
-        <li>{statistics.viewCount} views</li>
+        <li className="font-sans">{prettifyNumber(views)} views</li>
       </ul>
     </div>
   );
 };
 
 export default VideoCard;
-
