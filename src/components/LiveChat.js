f4d212332ab58b1c574d4e84efbf3ad16707ebd7
Anandagadugin <gaduginanand984@gmail.com>
2023-06-10 18:17:06 +0530

    Built Live chat feature using randmon names/Msg's

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,16 +1,67 @@
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
+import { useDispatch, useSelector } from "react-redux";
+import { addMessage } from "../utils/chatSlice";
+import store from "../utils/store";
+import { generateRandoMessages, generateRandomNames } from "../utils/helper";
 
 const LiveChat = () => {
+  const dispatch = useDispatch();
+  const [LiveMessage, setLiveMessage] = useState("");
+
+  const ChatMessages = useSelector((store) => store.chat.messages);
+  useEffect(() => {
+    const i = setInterval(() => {
+      // API polling
+      // console.log("API polling");
+
+      dispatch(
+        addMessage({
+          name: generateRandomNames(),
+          message: generateRandoMessages(18),
+        })
+      );
+    }, 1500);
+    return () => clearInterval(i);
+  }, []);
   return (
-    <div className="w-full  h-[550px] ml-2 p-2 border border-black bg-gray-100 rounded-md shadow-md">
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-      <ChatMessage name="Anand g" message="This is working and expected! 🚀" />
-    </div>
+    <>
+      <div className="w-full  h-[550px] ml-2 p-2 border border-black bg-gray-100 rounded-md shadow-md overflow-y-scroll flex flex-col-reverse">
+        {/* Don't use the index as a key  */}
+        <div>
+          {ChatMessages.map((c, i) => (
+            <ChatMessage key={i} name={c.name} message={c.message} />
+          ))}
+        </div>
+      </div>
+
+      <form
+        className="w-full p-2 ml-2 border border-black rounded-md"
+        onSubmit={(e) => {
+          e.preventDefault();
+          // console.log("On submit", LiveMessage);
+          dispatch(
+            addMessage({
+              name: "You",
+              message: LiveMessage,
+            })
+          );
+          setLiveMessage("");
+        }}
+      >
+        <input
+          className="w-96 px-2 border border-black"
+          type="text"
+          value={LiveMessage}
+          onChange={(e) => {
+            setLiveMessage(e.target.value);
+          }}
+        />
+        <button className=" ml-3 px-2 border border-black bg-green-200 rounded-md">
+          send
+        </button>
+      </form>
+    </>
   );
 };
 
