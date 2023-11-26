2331ed5780fd339b5ec054bb91c1c0235c868e4e
Anandagadugin <gaduginanand984@gmail.com>
2023-11-26 11:20:27 +0530

    API update

--- src/utils/contants.js
+++ src/utils/contants.js
@@ -1,10 +1,10 @@
 const GOOGLE_API_KEY = "AIzaSyBFz6YqSvjZ6y1q34A49j8xJYpjaD_738U";
 
 export const YOUTUBE_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
+  "https://corsproxy.io/?https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=" +
   GOOGLE_API_KEY;
 
 export const YOUTUBE_SEARCH_API =
-  "https://clients1.google.com/complete/search?client=firefox&ds=yt&q=";
+  "https://corsproxy.io/?https://clients1.google.com/complete/search?client=firefox&ds=yt&q=";
 
 export const OFFSET_LIVE_CHAT = 20;
